c028890d9b6286cfb5894d04288f14cf87cf974a
weavermarquez <weavermarquez0@gmail.com>
2025-09-23 10:46:16 -0400

    feat: display gameboard in grid w/o state

--- tictactoe/src/App.tsx
+++ tictactoe/src/App.tsx
@@ -3,6 +3,39 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+function Game() {
+  const [gamestate, setGamestate] = useState(null)
+
+  return (
+    <>
+      <div id="game">
+        <div id="cells">
+          <div id="rows" className="flex-col">
+            <div id="row1" className="flex">
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+            </div>
+            <div id="row2" className="flex">
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+            </div>
+            <div id="row3" className="flex">
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+              <div id="cell" className="w-33">[]</div>
+            </div>
+          </div>
+        </div>
+        <div id="status">Current Player: X</div>
+      </div>
+    </>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,27 +44,8 @@ function App() {
       <div>
         <h1 id="title" className="font-bold underline">Tic Tac Toe</h1>
       </div>
-      <div className="game">
-        <div className="cells">
-          <div className="rows">
-            <div className="row1">
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-            </div>
-            <div className="row2">
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-            </div>
-            <div className="row3">
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-              <div className="cell">[]</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Game />
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
