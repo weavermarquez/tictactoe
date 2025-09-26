4e3d4ac64a0c34841ad2aab9c29b9ed4f4db0bdf
weavermarquez <weavermarquez0@gmail.com>
2025-09-26 12:30:28 -0400

    draft(db): config drizzle, practice schema
    - drizzle.config.ts with default recommended configuration
    - .env gathered from a tutorial and Supabase
    - ./src/db/schema.ts and index.ts for practice

---
+++ tictactoe/src/index.ts
@@ -0,0 +1,47 @@
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+
+import { eq } from 'drizzle-orm';
+import { usersTable } from './db/schema';
+
+const connectionString = process.env.DATABASE_URL!
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    age: 30,
+    email: 'john@example.com',
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log('User info updated!')
+
+  await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  console.log('User deleted!')
+}
+
+main();
